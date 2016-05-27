2e1caec4b75b12d25879fd8d525e323bc2e09edb
Gabriel Birke <gabriel.birke@wikimedia.de>
2016-05-27 11:34:02 +0200

    Add example stepping

--- src/js/codeslide.js
+++ src/js/codeslide.js
@@ -17,30 +17,55 @@ function nextSlide() {
 }
 
 function prevExample() {
-    console.log("prevExample");
+    var currentSlide = jQuery( '#codeslides section.current' ),
+        currentExample = currentSlide.find( '.example.current' ),
+        prev = currentExample.prevAll( '.example' ).first();
+    if ( prev.length ) {
+        prev.addClass( 'current' );
+        currentExample.removeClass( 'current' );
+    } else if ( !currentExample.length ) {
+        currentSlide.find( '.example:last-child' ).addClass( 'current' );
+    } else {
+        currentExample.removeClass( 'current' );
+    }
 }
 
 function nextExample() {
-    console.log("nextExample");
+    var currentSlide = jQuery( '#codeslides section.current' ),
+        currentExample = currentSlide.find( '.example.current' ),
+        next = currentExample.nextAll( '.example' ).first();
+    if ( next.length ) {
+        next.addClass( 'current' );
+        currentExample.removeClass( 'current' );
+    } else if ( !currentExample.length ) {
+        console.log(currentSlide, currentSlide.find( '.example' ), currentSlide.find( '.example' ).first());
+        currentSlide.find( '.example' ).first().addClass( 'current' );
+    } else {
+        currentExample.removeClass( 'current' );
+    }
 }
 
 function init($) {
     $( '#codeslides section:first-child' ).addClass( 'current' );
     $(document).keydown(function(e) {
-            // The key codes are numpad numbers
+            // The key codes > 50 are numpad numbers
         switch(e.keyCode) {
-            case 100: // left
+            case 37: // left
+            case 100:
                 prevSlide();
                 break;
 
+            case 38:
             case 104: // up
                 prevExample();
                 break;
 
+            case 39:
             case 102: // right
                 nextSlide();
                 break;
 
+            case 40:
             case 98: // down
                 nextExample();
                 break;
